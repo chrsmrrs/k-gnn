a757af80b367e76b78e8fca385f0bbdc6e90b90b
Christopher Morris <christopher.morris@tu-dortmund.de>
2020-05-18 13:02:14 -0400

    MC.

--- examples/1-2-qm9_all_targets.py
+++ examples/1-2-qm9_all_targets.py
@@ -9,8 +9,8 @@ from torch_geometric.datasets import QM9
 import torch_geometric.transforms as T
 from torch_geometric.nn import NNConv
 from k_gnn import GraphConv, DataLoader, avg_pool
-from k_gnn import TwoMalkin, ConnectedThreeMalkin
-import numpy as np
+from k_gnn import TwoMalkin
+
 
 class MyFilter(object):
     def __call__(self, data):
@@ -22,17 +22,21 @@ class MyPreTransform(object):
         x = data.x
         data.x = data.x[:, :5]
         data = TwoMalkin()(data)
-        data = ConnectedThreeMalkin()(data)
         data.x = x
         return data
 
 
-path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', '1-27893y-QM9')
+
+
+
+
+path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', '1-2-QM9')
 dataset = QM9(
     path,
-    transform=T.Compose([T.Distance(norm=False)]),
+    transform=T.Compose([T.Distance()]),
     pre_transform=MyPreTransform(),
     pre_filter=MyFilter())
+
 dataset.data.y = dataset.data.y[:,0:12]
 
 dataset.data.iso_type_2 = torch.unique(dataset.data.iso_type_2, True, True)[1]
@@ -40,10 +44,6 @@ num_i_2 = dataset.data.iso_type_2.max().item() + 1
 dataset.data.iso_type_2 = F.one_hot(
     dataset.data.iso_type_2, num_classes=num_i_2).to(torch.float)
 
-dataset.data.iso_type_3 = torch.unique(dataset.data.iso_type_3, True, True)[1]
-num_i_3 = dataset.data.iso_type_3.max().item() + 1
-dataset.data.iso_type_3 = F.one_hot(
-    dataset.data.iso_type_3, num_classes=num_i_3).to(torch.float)
 
 
 
@@ -51,60 +51,46 @@ class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         M_in, M_out = dataset.num_features, 32
-        nn1 = Sequential(Linear(6, 128), ReLU(), Linear(128, M_in * M_out))
+        nn1 = Sequential(Linear(5, 128), ReLU(), Linear(128, M_in * M_out))
         self.conv1 = NNConv(M_in, M_out, nn1)
 
         M_in, M_out = M_out, 64
-        nn2 = Sequential(Linear(6, 128), ReLU(), Linear(128, M_in * M_out))
+        nn2 = Sequential(Linear(5, 128), ReLU(), Linear(128, M_in * M_out))
         self.conv2 = NNConv(M_in, M_out, nn2)
 
         M_in, M_out = M_out, 64
-        nn3 = Sequential(Linear(6, 128), ReLU(), Linear(128, M_in * M_out))
+        nn3 = Sequential(Linear(5, 128), ReLU(), Linear(128, M_in * M_out))
         self.conv3 = NNConv(M_in, M_out, nn3)
 
         self.conv4 = GraphConv(64 + num_i_2, 64)
         self.conv5 = GraphConv(64, 64)
 
-        self.conv6 = GraphConv(64 + num_i_3, 64)
-        self.conv7 = GraphConv(64, 64)
-
-
-        self.fc1 = torch.nn.Linear(3 * 64, 64)
+        self.fc1 = torch.nn.Linear(2 * 64, 64)
         self.fc2 = torch.nn.Linear(64, 32)
-        self.fc3 = torch.nn.Linear(32, 12)
+        self.fc3 = torch.nn.Linear(32, 1)
 
     def forward(self, data):
         data.x = F.elu(self.conv1(data.x, data.edge_index, data.edge_attr))
         data.x = F.elu(self.conv2(data.x, data.edge_index, data.edge_attr))
         data.x = F.elu(self.conv3(data.x, data.edge_index, data.edge_attr))
-        x = data.x
         x_1 = scatter_mean(data.x, data.batch, dim=0)
 
-        data.x = avg_pool(x, data.assignment_index_2)
+        data.x = avg_pool(data.x, data.assignment_index_2)
         data.x = torch.cat([data.x, data.iso_type_2], dim=1)
 
         data.x = F.elu(self.conv4(data.x, data.edge_index_2))
         data.x = F.elu(self.conv5(data.x, data.edge_index_2))
         x_2 = scatter_mean(data.x, data.batch_2, dim=0)
 
-        data.x = avg_pool(x, data.assignment_index_3)
-        data.x = torch.cat([data.x, data.iso_type_3], dim=1)
-
-        data.x = F.elu(self.conv6(data.x, data.edge_index_3))
-        data.x = F.elu(self.conv7(data.x, data.edge_index_3))
-        x_3 = scatter_mean(data.x, data.batch_3, dim=0)
-
-        x = torch.cat([x_1, x_2, x_3], dim=1)
+        x = torch.cat([x_1, x_2], dim=1)
 
         x = F.elu(self.fc1(x))
         x = F.elu(self.fc2(x))
         x = self.fc3(x)
-        return x
+        return x.view(-1)
 
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-
 
 results = []
 results_log = []
